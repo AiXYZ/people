36e84a9de5aeb2629f55f324709a4a03e34d5742
alialsayegh <aliakhtar@sayeghwater.com>
2017-10-14 11:33:01 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # people
 Search Engine
+
+# http://peoplealpha.azurewebsites.net/
---
+++ js/map/people-locations.js
@@ -0,0 +1,35 @@
+var locations = [
+    ['Ali Akhtar Mohammed', "3225 George Street, Las Vegas", "verified", 36.169941, -115.139830, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Adnan Ahmad', "3225 George Street, Los Angeles", "verified", 34.052234, -118.243685, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Genesis B. Lista', "3225 George Street, San Francisco", "verified", 37.774929, -122.419416, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Anjali Singh', "3225 George Street, San Jose", "verified", 37.338208, -121.886329, "img/people_photos/people_female_big.png", "img/map/people_female.png"],
+    
+    ['Sumant Kumar', "3225 George Street, Phoenix", "verified", 33.448377, -112.074037, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Beauty Sinha', "3225 George Street, Okalahoma", "verified", 35.007752, -97.092877, "img/people_photos/people_female_big.png", "img/map/people_female.png"],
+    ['Salim Mohammad', "3225 George Street, Houston", "verified", 29.760427, -95.369803, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Antara Das', "3225 George Street, Texas", "verified", 31.968599, -99.901813, "img/people_photos/people_female_big.png", "img/map/people_female.png"],
+    
+    ['Bhavesh Kumar', "3225 George Street, Salt Lake City", "verified", 40.760779, -111.891047, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Muzaffar Jamal Rana', "3225 George Street, Florida", "verified", 27.664827, -81.515754, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Ankita Srivastava', "3225 George Street, Orlando", "verified", 28.538335, -81.379236, "img/people_photos/people_female_big.png", "img/map/people_female.png"],
+    ['Afsar Alam', "3225 George Street, Kansas City", "verified", 39.099727, -94.578567, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    
+    ['Vicky Vishal', "3225 George Street, Missouri", "verified", 37.964253, -91.831833, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Riya Sheety', "3225 George Street, Cicago", "verified", 41.878114, -87.629798, "img/people_photos/people_female_big.png", "img/map/people_female.png"],
+    ['Atif Alam', "3225 George Street, Indiana", "verified", 40.267194, -86.134902, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Jyotir Moy Chatterjee', "3225 George Street, Kentucky", "verified", 37.839333, -84.270018, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+
+    ['Wasim Tahir', "3225 George Street, Mississippi", "verified", 32.354668, -89.398528, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Haimanti Das', "3225 George Street, Charlotte", "verified", 35.227087, -80.843127, "img/people_photos/people_female_big.png", "img/map/people_female.png"],
+    ['Girish Eati', "3225 George Street, Orlando", "verified", 28.538335, -81.379236, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Hemant Kumar Singh', "3225 George Street, Virginia", "verified", 37.431573, -78.656894, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    
+    ['Sulangan Jha', "3225 George Street, Philadelphia", "verified", 39.952584, -75.165222, "img/people_photos/people_female_big.png", "img/map/people_female.png"],
+    ['Achyut Mishra', "3225 George Street, Colorado", "verified", 39.550051, -105.782067, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Vidya Prakash', "3225 George Street, San Diego", "verified", 32.715738, -117.161084, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Abhishek Jha', "3225 George Street, Lubbock", "verified", 33.577863, -101.855166, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    
+    ['Ankur Jaiswal', "3225 George Street, Amarillo", "verified", 35.221997, -101.831297, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+    ['Avinash Singh', "3225 George Street, Albuqueerque", "verified", 35.085334, -106.605553, "img/people_photos/people_male_big.png", "img/map/people_male.png"],
+
+];
