d19f9d2049b6a9a73a84aa031015292df0ae941e
alialsayegh <aliakhtar@sayeghwater.com>
2017-10-15 14:27:16 +0300

    html to php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="people">
-	<meta name="keywords" content="people, search, engine">
+	<meta name="description" content="People">
+	<meta name="keywords" content="People, Search, Engine">
 	<meta name="author" content="Eng. Ali">    
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>people</title>
+    <title>People</title>
 	<!-- Favicon -->
 	<link rel="shortcut icon" href="img/favicon.ico">
 
---
+++ profile.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="people">
+	<meta name="keywords" content="People, Search, Engine">
+	<meta name="author" content="Eng. Ali">    
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>People</title>
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="img/favicon.ico">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+
+
+    <!-- JavaScript plugins -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>	  
+  </body>
+</html>
