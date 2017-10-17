ad1e612940925e98f7115a28257a902014bfb194
alialsayegh <aliakhtar@sayeghwater.com>
2017-10-17 16:07:46 +0300

    Prifle nav

--- profile.php
+++ profile.php
@@ -1,10 +1,24 @@
+<?php 
+/*
+http://localhost:8080/people/profile.php?id=100013489710942
+http://localhost:8080/people/EngAli 
+username minimum length is 5 
+ 
+ */
+if(!empty($_GET['id'])){
+	$username = $_GET['id'];
+}else{
+	$username = "no";
+}
+//echo $username;
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="people">
+	<meta name="description" content="People">
 	<meta name="keywords" content="People, Search, Engine">
 	<meta name="author" content="Eng. Ali">    
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
@@ -14,22 +28,64 @@
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
   </head>
-  <body>
-	<!-- http://localhost:8080/people/profile.php?id=100013489710942 -->
-	<!-- http://localhost:8080/people/EngAli -->
-	<?php 
-    if(!empty($_GET['id'])){
-    	$user = $_GET['id'];
-    }else{
-    	$user = "no";
-    }
-	?>
-	
-	<h1><?php echo $user; ?></h1>
+
+  <body class="bodyClass">
+
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src="img/people-logo.png" alt="People"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right">
+		    <div class="input-group">
+		      <input type="text" class="form-control">
+		      <span class="input-group-btn">
+		        <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+		      </span>
+		    </div><!-- /input-group -->            
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; 2016 Company, Inc.</p>
+      </footer>
+    </div> <!-- /container -->
 
     <!-- JavaScript plugins -->
     <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>	  
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
