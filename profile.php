67678bb0bdee440dbcf7fb9dd99f56665fa565ef
alialsayegh <aliakhtar@sayeghwater.com>
2017-10-26 14:33:49 +0300

    work and contact

--- profile.php
+++ profile.php
@@ -28,6 +28,8 @@ $profileUrl = "http://peoplealpha.azurewebsites.net/" . $username;
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
+	<!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"> -->
+	<link href="css/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
   </head>
 
   <body class="bodyClass">
@@ -126,66 +128,82 @@ $profileUrl = "http://peoplealpha.azurewebsites.net/" . $username;
 	  <h3>Work</h3>
 	  <div class="media">
 	    <div class="media-left media-top">
-	      <img src="img/education/stanford.png" class="media-object" style="width:80px">
+	      <img src="img/work/qmax.png" class="media-object" style="width:80px">
 	    </div>
 	    <div class="media-body">
-	      <h4 class="media-heading">Stanford University</h4>
-	      <p>MS - Innovation and Entrepreneurship</p>
+	      <h4 class="media-heading">Qmax Systems</h4>
+	      <p>Software Engineer</p>
+	      <p>2013 - 2015</p>
+	      <p>
+			Responsible for developing and coding web and desktop-based software.
+			Wrote well designed, testable, efficient code that meets specifications and follows standards.
+			Develop solution architecture based from project requirements.
+			Design and develop web services, API.
+			Modify existing software to correct errors, allow it to adapt to new hardware, and to improve its performance.
+			Integrate applications using components develop in-house and third party libraries.
+			Participate in day-to-day coding, performance profiling, optimization, and general troubleshooting.
+			Integrate software components into a fully functional software system.
+			Analyze and resolve technical and application problems.
+			Assist in the collection and documentation of user's requirements, development of user stories, and estimates.
+	      </p>
+	    </div>
+	  </div>
+	  <hr>
+	  <div class="media">
+	    <div class="media-left media-top">
+	      <img src="img/work/emc.png" class="media-object" style="width:80px">
+	    </div>
+	    <div class="media-body">
+	      <h4 class="media-heading">EMC</h4>
+	      <p>Student Ambassador</p>
 	      <p>2013 - 2014</p>
 	      <p>
-			A greater understanding of the tools that every innovative organization needs to succeed.
-			A wider and deeper set of innovation skills, customized to fit both your organization’s needs and your career goals.
-			A greater understanding the best and most current research on innovation and entrepreneurship.
-			Deeper insights into the people you are serving - a powerful tool for innovation.
-			The joy of learning from and engaging with some of Stanford’s smartest and most entertaining teachers
-			Personal feedback and advice from Stanford’s team of innovation coaches.
+			Serve as a bridge between EMC Corporation and Universities. 
+			Conduct seminars and organize events to create awareness about EMC and EMC Academic Alliance Program. 
+			Participate in the events on behalf of EMC Corporation.
 	      </p>
 	    </div>
 	  </div>
 	  <hr>
 	  <div class="media">
 	    <div class="media-left media-top">
-	      <img src="img/education/mgr.png" class="media-object" style="width:80px">
+	      <img src="img/work/microsoft.png" class="media-object" style="width:80px">
 	    </div>
 	    <div class="media-body">
-	      <h4 class="media-heading">Dr. M.G.R. University</h4>
-	      <p>B. Tech - Computer Science And Engineering</p>
-	      <p>2009 - 2013</p>
-	      <p>
-			Was awarded the First Prize Winner for project zoom by Mr. Shibu Thomas, Associate Director of Projects - Cognizant. 
-			Winner of IBM Master the Mainframe Contest 2012 and eligible to be in first 200 candidates to pass Part-2.
-	      </p>
-	    </div>
-	  </div>
-	  <hr>
-	  <div class="media">
-	    <div class="media-left media-top">
-	      <img src="img/education/university.png" class="media-object" style="width:80px">
-	    </div>
-	    <div class="media-body">
-	      <h4 class="media-heading">Kamla Rai College</h4>
-	      <p>Senior Secondary - Mathematics and Science</p>
-	      <p>2007 - 2009</p>
+	      <h4 class="media-heading">Microsoft</h4>
+	      <p>MSP</p>
+	      <p>2013 - 2013</p>
 	      <p></p>
 	    </div>
-	  </div>	  
+	  </div>
+	  
+	  <!-- Contact -->
+	  <h3>Contact</h3>	  
+	  <div class="row">
+	    <div class="col-md-6">
+			<address>
+			  <strong>GICT, Inc.</strong><br>
+			  181428 Gopalganj, Bihar<br>
+			  Mobile Phone: +91 90430 38157<br>
+			  Email: EngAliAkhtar@gmail.com
+			</address>	      
+	    </div>
+	    <div class="col-md-6">
+			<ul class="list-unstyled">
+			  <li><span class="glyphicon glyphicon-globe"></span> <a href="<?php echo $profileUrl ?>"><?php echo $profileUrl ?></a></li>
+			  <li><i class="fa fa-facebook-official"></i> <a href="https://www.facebook.com/EngAliAkhtar"> https://www.facebook.com/EngAliAkhtar </a></li>
+			  <li><i class="fa fa-twitter-square"></i> <a href="https://www.facebook.com/EngAliAkhtar"> https://www.facebook.com/EngAliAkhtar </a></li>
+			  <li><i class="fa fa-instagram"></i> <a href="https://www.facebook.com/EngAliAkhtar"> https://www.facebook.com/EngAliAkhtar </a></li>
+			  <li><i class="fa fa-linkedin-square"></i> <a href="https://www.facebook.com/EngAliAkhtar"> https://www.facebook.com/EngAliAkhtar </a></li>
+			</ul>	      
+	    </div>
+	  </div>	  	  
 	</div>
-
-
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <p>Engineer, Public Speaker, Political Critic</p>
-          <p><span class="glyphicon glyphicon-search"></span> Riyadh, Saudi Arabia</p>
-          <a href="<?php echo $profileUrl ?>"><?php echo $profileUrl ?></a>
-        </div>
-      </div>
-    </div> <!-- /container --> 
     
     <div class="container">
       <hr>
       <footer>
-        <p>&copy; 2016 Company, Inc.</p>
+        <p>&copy; 2017 People, Inc.</p>
       </footer>
     </div> <!-- /container -->    
 
